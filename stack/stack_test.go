593de1e6cefccbc5609ad53983c2e4cc4dcf10b9
Gabe <gprice0105@gmail.com>
2024-06-26 22:49:09 -0400

    added ClearStack method

--- stack/stack_test.go
+++ stack/stack_test.go
@@ -2,13 +2,16 @@ package stack
 
 import "testing"
 
+var stack = Stack{nil, 0}
+
 func TestStack(t *testing.T) {
-	stack := Stack{nil, 0}
 
 	t.Run("Testing Push function", func(t *testing.T) {
 
+		ClearStack(&stack)
+
 		Push(&stack, 1)
-		Push(&stack, 1)
+		Push(&stack, 2)
 		Push(&stack, 3)
 
 		stackHeadVal := stack.head.value
@@ -19,8 +22,10 @@ func TestStack(t *testing.T) {
 	})
 	t.Run("Testing pop function", func(t *testing.T) {
 
+		ClearStack(&stack)
+
 		Push(&stack, 1)
-		Push(&stack, 1)
+		Push(&stack, 2)
 		Push(&stack, 3)
 
 		Pop(&stack)
@@ -30,6 +35,22 @@ func TestStack(t *testing.T) {
 
 		if stackHeadVal != 1 {
 			t.Errorf("Want head: %d || Got head: %d", 1, stackHeadVal)
+		}
+	})
+	t.Run("Testing ClearStack function", func(t *testing.T) {
+
+		ClearStack(&stack)
+
+		Push(&stack, 1)
+		Push(&stack, 2)
+		Push(&stack, 3)
+		Push(&stack, 4)
+		Push(&stack, 5)
+
+		ClearStack(&stack)
+
+		if stack.head != nil || stack.length != 0 {
+			t.Errorf("Want: Nil stack and 0 length. Got: Stack head: %v and stack length: %d", stack.head, stack.length)
 		}
 	})
 }
